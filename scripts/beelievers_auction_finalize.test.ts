310041b841e851228759a9b6dd9b61ac43710e89
Robert Zaremba <robert@zaremba.ch>
2025-08-17 19:16:03 +0200

    update tsconfig and add example beelievers_auction_finalize.ts
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- scripts/beelievers_auction_finalize.test.ts
+++ scripts/beelievers_auction_finalize.test.ts
@@ -1,5 +1,7 @@
 import { expect, test, describe } from "vitest";
 
+import { sortAddresses } from "./beelievers_auction_finalize";
+
 // sample function for Vu
 function sum(a: number, b: number) {
 	return a + b;
@@ -8,5 +10,10 @@ function sum(a: number, b: number) {
 describe("sort addresses", () => {
 	test("adds 1 + 2 to equal 3", () => {
 		expect(sum(1, 2)).toBe(3);
+	});
+
+	test("sorts single address", () => {
+		const addr = "0x0123";
+		expect(sortAddresses([addr])).toEqual([addr]);
 	});
 });
---
+++ scripts/beelievers_auction_finalize.ts
@@ -0,0 +1,7 @@
+export function sortAddresses(addrs: string[]): string[] {
+	// TODO: convert address to a Sui address object
+	// make mapping to number
+	// sort by number
+	// convert back to list
+	return addrs;
+}
